d4c5c9cae73fe5e231eeb038fba1e3f05ddb075f
Erik Petrov <erik.petrov@ut.ee>
2025-03-26 17:20:29 +0200

    Initial commit

---
+++ src/Checksum.java
@@ -0,0 +1,3 @@
+public class Checksum extends File {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("File path: ");
+        String p = s.nextLine();
+        System.out.println("Checksum to control: ");
+        String c = s.nextLine();
+
+    }
+}
