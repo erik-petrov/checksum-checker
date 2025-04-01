d20cfd24c15124088f790d048333191d9df563ad
deljansk <ideljanski@gmail.com>
2025-04-01 20:15:53 +0300

    Tegin File klassi

--- src/File.java
+++ src/File.java
@@ -1,3 +1,20 @@
-public class File {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class File {
+    public static byte[] loebaite(String failinimi) throws IOException{
+        return Files.readAllBytes(Paths.get(failinimi));
+    }
+
+    public static String loesõnu(String failinimi) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(failinimi)));
+    }
+    public static void salvestatulemusfaili(String checksum, String failinimi2) throws IOException{
+        Files.write(Paths.get(failinimi2), checksum.getBytes());
+    }
+    public static boolean kasfailonolemas (String failinimi){
+        return Files.exists(Paths.get(failinimi));
+    }
 }
