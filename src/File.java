5e03d9fcc6899f5bbceca02eae157ae4bf7662f7
deljansk <ideljanski@gmail.com>
2025-04-03 00:27:17 +0300

    k

--- src/File.java
+++ src/File.java
@@ -4,17 +4,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class File {
-    public static byte[] loebaite(String failinimi) throws IOException{
+
+    public String path;
+    public String failinimi;
+    public static byte[] loeBaite(String failinimi) throws IOException{
         return Files.readAllBytes(Paths.get(failinimi));
     }
 
-    public static String loesõnu(String failinimi) throws IOException {
+    public static String loeSõnu(String failinimi) throws IOException {
         return new String(Files.readAllBytes(Paths.get(failinimi)));
     }
-    public static void salvestatulemusfaili(String checksum, String failinimi2) throws IOException{
+    public static void salvestaTulemusFaili(String checksum, String failinimi2) throws IOException{
         Files.write(Paths.get(failinimi2), checksum.getBytes());
     }
-    public static boolean kasfailonolemas (String failinimi){
+    public static boolean kasFailOnOlemas (String failinimi){
         return Files.exists(Paths.get(failinimi));///
     }
 }
